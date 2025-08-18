7e3925101b888075326f4e8db7caeb64e7a24755
WooChan Sun <65938333+SunWooChan@users.noreply.github.com>
2025-08-18 13:34:10 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -2,26 +2,24 @@ n = int(input())
 count = 0
 
 for _ in range(n):
-	word = input()
-	tmp = []
 	flag = True
+	word = input()
+	word_set = set()
+	tmp = word[0]
+	word_set.add(tmp)
 
-	for w in word:
-		
-		if len(tmp)==0:
-			tmp.append(w)
-		
-		elif w == tmp[-1]:
+	for i in word[1:]:
+		# tmp와 같을 경우
+		if i == tmp:
 			continue
-
-		elif w in tmp:
-			flag = False
-			break
-
-		else:
-			tmp.append(w)
-
-	if flag == True:
-		count+=1
+		# tmp와 다를 경우
+		elif i != tmp:
+			if i in word_set:
+				flag = False
+				break
+			word_set.add(i)
+			tmp = i
+	if flag:
+		count += 1
 
 print(count)
