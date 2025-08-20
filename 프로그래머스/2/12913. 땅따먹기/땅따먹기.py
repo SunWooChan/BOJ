4775e6f66af3858d44b1892e66c33b2aca76f7e5
WooChan Sun <65938333+SunWooChan@users.noreply.github.com>
2025-08-20 16:47:09 +0900

    [level 2] Title: 땅따먹기, Time: 577.71 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,18 +1,13 @@
 def solution(land):
     n = len(land)
+    dp = [[0] * 4 for i in range(n)]
     
-    # dp table
-    dp = [[0] * 4 for _ in range(n)] 
-    
-    # dp 초기화
-    dp[0] = land[0][:]
+    dp[0] = land[0]
     
     for i in range(1, n):
         for j in range(4):
             for k in range(4):
                 if k != j:
                     dp[i][j] = max(dp[i][j], dp[i-1][k] + land[i][j])
-    
     answer = max(dp[n-1])
-    
     return answer
